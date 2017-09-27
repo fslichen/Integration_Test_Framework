202433d66cb639eaaabd2efd718df5810e708ed9
chenli <chenli@chens-air>
2017-09-27 11:36:08 +0800

    new demos

--- Integration_Test_Framework/src/main/java/evolution/Application.java
+++ Integration_Test_Framework/src/main/java/evolution/Application.java
@@ -1,27 +1,11 @@
 package evolution;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import evolution.dao.AnyDao;
-import evolution.entity.AnyEntity;
-
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-	@Autowired
-	private AnyDao anyDao;
-	
+public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
-	}
-
-	@Override
-	public void run(String... arg0) throws Exception {
-		AnyEntity anyEntity = new AnyEntity();
-		anyEntity.setName("Ling");
-		anyDao.insert(anyEntity);
-		System.out.println(anyDao.findAll());
 	}
 }
